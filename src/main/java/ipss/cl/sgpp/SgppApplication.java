2b7f0a9c2cc72ad803cfb84c5b13fbfe3be57342
felipeDev303 <felipe.morales.roa@ciisa.cl>
2025-12-07 23:19:35 -0300

    Initial commit

---
+++ src/main/java/ipss/cl/sgpp/SgppApplication.java
@@ -0,0 +1,13 @@
+package ipss.cl.sgpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipss/cl/sgpp/SgppApplicationTests.java
@@ -0,0 +1,13 @@
+package ipss.cl.sgpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
